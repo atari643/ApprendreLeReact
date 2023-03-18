be4ced84b162367638b04d502497b2f17b5bc8a9
qartigala <quentin.artigala@etu.u-bordeaux.fr>
2023-03-18 13:16:58 +0100

    Création d'un composant react

--- app.js
+++ app.js
@@ -1,26 +1,46 @@
-let n = 0
-function numberFormat(n){
-    return n.toString().padStart(2, '0')
-}
-function render(){
-    const items=[
-        'Tache 1',
-        'Tache 2',
-        'Tache 3'
-    ]
-    const lis = items.map((item,k)=><li key={k}>{item}</li>)
-    const title = <React.Fragment><h1 className="title" id="title">
-        Bonjour les gens <span>{n}</span>
-    </h1>
-    <ul>
-        {lis}
-    </ul>
-    </React.Fragment>
+// let n = 0
+// function numberFormat(n) {
+//     return n.toString().padStart(2, '0')
+// }
+// function render() {
+//     const items = [
+//         'Tache 1',
+//         'Tache 2',
+//         'Tache 3'
+//     ]
+//     const lis = items.map((item, k) => <li key={k}>{item}</li>)
+//     const title = <React.Fragment><h1 className="title" id="title">
+//         Bonjour les gens <span>{n}</span>
+//     </h1>
+//         <ul>
+//             {lis}
+//         </ul>
+//     </React.Fragment>
 
-    ReactDOM.render(title, document.querySelector('#app'))
+//     ReactDOM.render(title, document.querySelector('#app'))
+// }
+// render()
+// window.setInterval(() => {
+//     n++
+//     render();
+// }, 1000)
+
+function WelcomeFunc({name, children}){
+
+    return <div>
+        <h1>Bonjour {name}</h1>
+        <p>{children}</p>
+    </div>
 }
-render()
-window.setInterval(()=>{
-    n++
-    render();
-}, 1000)
+class Welcome extends React.Component{
+    constructor(props) {
+        super(props)
+    }
+    render(){
+        return <div>
+        <h1>Bonjour {this.props.name}</h1>
+        <p>{this.props.children}</p>
+    </div>
+    }
+}
+ReactDOM.render(<Welcome name='Michel'>Bonjour tout le monde</Welcome>, document.querySelector('#app'));
